a91cb7fae7341cd0b87412b904c8b8c0dbb4ad38
Lodis Perkins <s188043@students.aie.edu>
2018-11-28 17:06:36 -0600

    shop

--- project2D/Shop.cpp
+++ project2D/Shop.cpp
@@ -10,36 +10,44 @@ Shop::Shop(std::vector<DefenseItem> armor, std::vector<AttackItem> attack)
 	armorstock = armor;
 	attackstock = attack;
 }
-const char * Shop::speak(int num)
+const char * Shop::update(int num)
 {
 	const char* say;
 	switch (shopstate)
 	{
-	case(welcome):
-	{
-		say = "Asher: Ah! Well if it isn't the warrior who dare challanges Dameon. How may I aid you today my suicidal friend?";
-		return say;
-	}
-	case(viewAttackItems):
-	{
-		say = "Asher: Need a new move eh? I guess spamming the attack button wasn't enough?";
-		return say;
-	}
-	case(viewDefenseItems):
-	{
-		say = "Asher: Please enjoy one of our mostly legal premium performance enhancing elixers.";
-		return say;
-	}
-	case(viewUpgrades):
-	{
-		say = "Asher: So you need to get more physically fit? Hehe, why workout when you can pay me?";
-		return say;
-	}
-	case(sell):
-	{
-		say = "Asher: Thank you,thank you! A fine choice my friend. Will you be buying more?";
-		return say;
-	}
+		case(welcome):
+		{
+			if (ImGui::Button("Attack Items", ImVec2(100,100)))
+			{
+				shopstate = viewAttackItems;
+			}
+			if (ImGui::Button("Defense Items", ImVec2(100, 100)))
+			{
+				shopstate = viewDefenseItems;
+			}
+			say = "Asher: Ah! Well if it isn't the warrior who dare challanges Dameon. How may I aid you today?";
+			return say;
+		}
+		case(viewAttackItems):
+		{
+			say = "Asher: Need a new move eh? I guess spamming the attack button wasn't enough?";
+			return say;
+		}
+		case(viewDefenseItems):
+		{
+			say = "Asher: Please enjoy one of our mostly legal premium performance enhancing elixers.";
+			return say;
+		}
+		case(viewUpgrades):
+		{
+			say = "Asher: So you need to get more physically fit? Hehe, why workout when you can pay me?";
+			return say;
+		}
+		case(sell):
+		{
+			say = "Asher: Thank you,thank you! A fine choice my friend. Will you be buying more?";
+			return say;
+		}
 	}
 }
 AttackItem Shop::sellattack(int choice)
@@ -76,10 +84,11 @@ void Shop::draw(aie::Renderer2D*renderer, int timer,aie::Font* font)
 {
 	renderer->setUVRect(0.f, 0.f, 1, 1);
 	renderer->drawSprite(shop, 600, 450, 500, 583);
+
 	renderer->drawSprite(hero, 600, 400, 57, 92);
 
 	renderer->setUVRect(int(timer * 10) % 3 / 2.9f, 0.f, .33, 1);
 	renderer->drawSprite(shopkeep, 600, 720 - 70, 38, 54);
 
-	renderer->drawText(font, speak(shopstate), 300, 100, 100);
+	renderer->drawText(font, update(shopstate), 300, 100, 100);
 }
